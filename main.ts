e47abb432852f591509fcb59d54711b969b4c249
Henry Gustafson <hgustafson@protonmail.com>
2022-09-30 14:06:00 -0700

    add regex

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
-import { App, Plugin, PluginSettingTab, MarkdownView } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, MarkdownView } from 'obsidian';
 import * as CodeMirror from "codemirror";
 
 interface PluginSettings {
-
+	shouldRegex: boolean;
+	regex: string;
+	regexReplace: string;
 }
 
 const DEFAULT_SETTINGS: PluginSettings = {
-
+	shouldRegex: false,
+	regex: '',
+	regexReplace: ''
 }
 
 interface SelectionRange {
@@ -62,7 +66,7 @@ export default class OrderList extends Plugin {
 
 			let linesList = [];
 			for (let i = 0; i < lines.length; i++) {
-				linesList.push([lines[i],eval(lines[i].split("\n")[0].split(" ").pop())]);
+				linesList.push([lines[i], this.evaluateLineValue(lines[i].split("\n")[0].split(" ").pop())]);
 			}
 
 			linesList.sort(function (a, b) { return a[1] - b[1] })
@@ -108,6 +112,34 @@ export default class OrderList extends Plugin {
 		};
 	}
 
+	evaluateLineValue(line: string) {
+		console.log(line)
+		if (this.settings.shouldRegex == false) {
+			try {
+				return eval(line);
+			} catch {
+				return 999999;
+			}
+		} else {
+			try {
+				var re = new RegExp(this.settings.regex, '');
+				return eval(line.replace(re, this.settings.regexReplace));
+				// var re = new RegExp('(\d+)(.)','');
+				// return eval(line.replace(re, '$1'));
+			} catch {
+				console.log("regex error")
+				return 999999;
+			}
+		}
+		
+
+		// if (line = "") {
+		// 	return 99999;
+		// } else {
+		// 	return eval(line)
+		// }
+	}
+
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -133,8 +165,27 @@ class SettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', { text: 'Order List (no settings currently)' });
+		containerEl.createEl('h2', { text: 'Order List Settings' });
 
+		new Setting(containerEl)
+			.setName("Use regex to evaluate line's number")
+			.addToggle(cb => cb.onChange(value => {
+				this.plugin.settings.shouldRegex = value;
+				this.plugin.saveSettings();
+			}).setValue(this.plugin.settings.shouldRegex));
 
+		new Setting(containerEl)
+			.setName("Add your own regex")
+			.addText(cb => cb.onChange(value => {
+				this.plugin.settings.regex = value;
+				this.plugin.saveSettings();
+			}).setValue(this.plugin.settings.regex));
+
+		new Setting(containerEl)
+			.setName("Replace")
+			.addText(cb => cb.onChange(value => {
+				this.plugin.settings.regexReplace = value;
+				this.plugin.saveSettings();
+			}).setValue(this.plugin.settings.regexReplace));
 }
 }
